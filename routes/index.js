2995cd6bbf50e8935481594da65340fc8e1e70ce
renatodiasdev <j_renatodias@hotmail.com>
2016-07-25 10:34:50 -0300

    incluindo arquivos da api

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var App = express.Router();
+var usuarios = getmodule('/node-projects/rdsi/api/usuario/usuario.js');
+
+/* GET home page. */
+App.get('/') 
+   .index(usuarios.index);
+
+App.route('/usuario')
+	.get(usuarios.read)
+	.post(usuarios.create);
+
+
+App.route('/usuario/:id')
+	.get(usuarios.profile)
+	.put(usuarios.update)
+	.delete(usuarios.delete);
+
+module.exports = App;
